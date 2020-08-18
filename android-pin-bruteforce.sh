eddaac6f060ee0c41878ceeaa4ef46942b234bfc
urbanadventurer <horton.nz@gmail.com>
2020-08-19 00:40:37 +1000

    Add comment header

--- android-pin-bruteforce.sh
+++ android-pin-bruteforce.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Android-HID-Bruteforce
+# 
+# Unlock an Android phone (or device) by bruteforcing the lockscreen PIN.
+# 
+# Turn your Kali Nethunter phone into a bruteforce PIN cracker for Android devices!
+# This uses the USB OTG cable to emulate a keyboard, automatically try PINs, and wait after trying too many wrong guesses.
 #
+# https://github.com/urbanadventurer/Android-HID-Bruteforce
 
 
 
